216117521537a00a1a0b5a472139a527e35e1b1f
Tanuj <tvasudev@alumni.cmu.edu>
2026-05-03 23:13:52 -0700

    zig modularization is familiar

--- src/read_input.zig
+++ src/read_input.zig
@@ -1,10 +1,7 @@
 const std = @import("std");
-const print = std.debug.print;
-const nl = "\n";
-
-fn printError(err: anyerror) void {
-    print("error: {s}" ++ nl, .{@errorName(err)});
-}
+const utils = @import("utils.zig");
+const print = utils.print;
+const nl = utils.nl;
 
 pub fn runTask(init: std.process.Init) !void {
     const io = init.io;
@@ -19,7 +16,7 @@ pub fn runTask(init: std.process.Init) !void {
             if (stdin.takeByte()) |b| {
                 if (b == '\n') break;
             } else |err| {
-                printError(err);
+                utils.printError(err);
                 break;
             }
         }
@@ -34,25 +31,8 @@ pub fn main(init: std.process.Init) !void {
     const start = std.Io.Clock.awake.now(io);
     // run task
     runTask(init) catch |err| {
-        printError(err);
+        utils.printError(err);
     };
     const elapsed = start.untilNow(io, .awake);
-    printTimeTaken(elapsed);
-}
-
-fn printTimeTaken(elapsed: std.Io.Duration) void {
-    const nanos = elapsed.toNanoseconds();
-    const million: f64 = 1_000_000.0;
-    const billion: f64 = 1_000_000_000.0;
-    if (nanos < 1_000_000) {
-        print("time taken: {d} nanoseconds" ++ nl, .{nanos});
-    } else if (nanos < 1_000_000_000) {
-        // Show precise ms, including fraction
-        const ms: f64 = @as(f64, @floatFromInt(nanos)) / million;
-        print("time taken: {d:.1} milliseconds", .{ms});
-    } else {
-        // Don't truncate to whole seconds: print fraction
-        const seconds: f64 = @as(f64, @floatFromInt(nanos)) / billion;
-        print("time taken: {d:.2} seconds", .{seconds});
-    }
+    utils.printTimeTaken(elapsed);
 }
--- src/utils.zig
+++ src/utils.zig
@@ -1,13 +1,35 @@
 const std = @import("std");
 const Io = std.Io;
-const nl = "\n";
+pub const nl = "\n";
 const DELIMITER = "---";
+pub const print = std.debug.print;
 
 pub fn debugPrintDelimiter() !void {
-    std.debug.print("---" ++ nl, .{});
+    print("{s}" ++ nl, .{DELIMITER});
 }
 
 pub fn flushDelimiter(writer: *Io.Writer) Io.Writer.Error!void {
     try writer.writeAll(DELIMITER ++ nl);
     try writer.flush();
+}
+
+pub fn printError(err: anyerror) void {
+    print("error: {s}" ++ nl, .{@errorName(err)});
+}
+
+pub fn printTimeTaken(elapsed: std.Io.Duration) void {
+    const nanos = elapsed.toNanoseconds();
+    const million: f64 = 1_000_000.0;
+    const billion: f64 = 1_000_000_000.0;
+    if (nanos < 1_000_000) {
+        print("time taken: {d} nanoseconds" ++ nl, .{nanos});
+    } else if (nanos < 1_000_000_000) {
+        // Show precise ms, including fraction
+        const ms: f64 = @as(f64, @floatFromInt(nanos)) / million;
+        print("time taken: {d:.1} milliseconds", .{ms});
+    } else {
+        // Don't truncate to whole seconds: print fraction
+        const seconds: f64 = @as(f64, @floatFromInt(nanos)) / billion;
+        print("time taken: {d:.2} seconds", .{seconds});
+    }
 }
